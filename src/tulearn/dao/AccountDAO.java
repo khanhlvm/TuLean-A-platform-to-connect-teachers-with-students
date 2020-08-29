d036dca67858cc2a7688099936778e2e205e463e
nhanntt13 <nguyenthithuynhan1999@gmail.com>
2020-08-29 17:09:30 +0700

    Register

--- src/tulearn/dao/AccountDAO.java
+++ src/tulearn/dao/AccountDAO.java
@@ -50,5 +50,21 @@ public class AccountDAO {
 		}
 		return result;
 	}
+	public boolean register(String email, String password) throws SQLException {
+		boolean result = false;
+		try {
+			conn = DBContext.getConnection();
+			if (conn != null) {
+				String sql = "INSERT INTO UserTB(email, password) VALUES(?,?)";
+				PreparedStatement ps = conn.prepareStatement(sql);
+				ps.setString(1, email);
+				ps.setString(2, password);
+				result = (ps.executeUpdate() > 0);
+			}
+		} finally {
+			closeConnection();
+		}
+		return result;
+	}
 	
 }
