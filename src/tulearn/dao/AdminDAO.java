4b30b6c5e07025957793034baeb2fc89930b21b0
vietpxde <vietpxde130059@fpt.edu.vn>
2020-08-30 19:23:18 +0700

    update funtion admin

--- src/tulearn/dao/AdminDAO.java
+++ src/tulearn/dao/AdminDAO.java
@@ -4,6 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import tulearn.context.DBContext;
+import tulearn.dto.Subject;
 
 public class AdminDAO {
 	private Connection conn;
@@ -23,7 +28,58 @@ public class AdminDAO {
 			conn.close();
 		}
 	}
-	
-	//Insert code below
-	
+
+	// Insert code below
+	public boolean insertSubject(String subjectName) throws SQLException {
+		boolean result = false;
+		try {
+			conn = DBContext.getConnection();
+			if (conn != null) {
+				String sql = "INSERT INTO SubjectTB(subjectName) VALUES (?)";
+				ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+				ps.setString(1, subjectName);
+				result = ps.executeUpdate() > 0;
+			}
+		} finally {
+			closeConnection();
+		}
+		return result;
+	}
+// show list subject
+	public ArrayList<Subject> getAllSubject() throws SQLException {
+		ArrayList<Subject> al = new ArrayList<>();
+		try {
+			conn = DBContext.getConnection();
+			if (conn != null) {
+				String sql = "SELECT * FROM SubjectTB";
+				ps = conn.prepareStatement(sql);
+				rs = ps.executeQuery();
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					String name = rs.getString(2);
+					al.add(new Subject(id, name));
+				}
+			}
+		} finally {
+			closeConnection();
+		}
+		return al;
+	}
+	//update status User
+	public boolean updateStatus(int statusID, String statusName) throws SQLException {
+		boolean result = false;
+		try {
+			conn = DBContext.getConnection();
+			if (conn != null) {
+				String sql = "UPDATE StatusTB SET statusID=? WHERE statusName = ?";
+				ps = conn.prepareStatement(sql);
+				ps.setString(1, statusName);
+				ps.executeUpdate();
+			}
+		} finally {
+			closeConnection();
+		}
+		return result;
+	}
+
 }
--- src/tulearn/dao/Main.java
+++ src/tulearn/dao/Main.java
@@ -1,15 +1,27 @@
 package tulearn.dao;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import tulearn.dto.Subject;
 import tulearn.dto.User;
 
 public class Main {
 
-	public static void main(String[] args) throws SQLException {
-		AccountDAO dao = new AccountDAO();
-		User u = dao.checkLogin("khanhlvm98@gmail.com", "khanh123456");
-		System.out.println("Successfully login, your id: " + u.getUserID());
-	}
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
+		AdminDAO dao = new AdminDAO();
+//		
+//		// Thêm môn học
+//		if (dao.insertSubject("Toán")) {
+//			System.out.println("Insert Success");	
+//		}else {
+//			System.out.println("Insert Fail");
+//		}
+		 //Lấy tất cả các môn học
+		ArrayList<Subject> al = dao.getAllSubject();
+		for (Subject i: al) {
+			System.out.println(i.getSubjectID() +" - "+ i.getSubjectName());
+		}
+	}
 }
